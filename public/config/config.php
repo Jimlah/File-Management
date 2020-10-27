2a344a6a5e158763e79dc7fc93d3ae4721ffd78c
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-10-27 16:54:39 +0100

    Update config.php

--- public/config/config.php
+++ public/config/config.php
@@ -107,5 +107,61 @@ class Database
         session_destroy(); // destroy session
         header("location:index.php"); 
     }
+
+    public function upload()
+    {
+        if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK) {
+            $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+            $fileName = $_FILES['uploadedFile']['name'];
+            $fileSize = $_FILES['uploadedFile']['size'];
+            $fileType = $_FILES['uploadedFile']['type'];
+            $fileNameCmps = explode(".", $fileName);
+            $fileExtension = strtolower(end($fileNameCmps));
+
+            $target_dir = "../../images/";
+            $target_file = $target_dir . basename($fileName);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+            if (move_uploaded_file($fileTmpPath, $target_file)) {
+                return "The file " . htmlspecialchars(basename($fileTmpPath)) . " has been uploaded.";
+            } else {
+                return "Sorry, there was an error uploading your file.";
+            }
+           
+        }
+        
+
+
+
+        // Check if file already exists
+        
+
+        
+
+        
+
+        // Check if $uploadOk is set to 0 by an error
+        // if ($uploadOk == 0) {
+        //     echo "Sorry, your file was not uploaded.";
+        //     // if everything is ok, try to upload file
+        // } else {
+        //     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        //         echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+        //     } else {
+        //         echo "Sorry, there was an error uploading your file.";
+        //     }
+        // }
+
+
+        // $sql = 'INSERT INTO `user`(`name`, `username`, `email`, `password`) VALUES (:name, :username, :email, :password)';
+        // $query = $this->dbh->prepare($sql);
+        // $query->bindParam(':name', $name, PDO::PARAM_STR);
+        // $query->bindParam(':username', $username, PDO::PARAM_STR);
+        // $query->bindParam(':email', $email, PDO::PARAM_STR);
+        // $query->bindParam(':password', $password, PDO::PARAM_STR);
+        // $query->execute();
+    }
+
 }
 
