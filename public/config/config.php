59582b39a430dd751aaa991d8feccf5e76c1729c
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-10-28 09:28:48 +0100

    Reply and Request Function implemented

--- public/config/config.php
+++ public/config/config.php
@@ -23,6 +23,8 @@ class Database
         }
     }
 
+    // To check if the email or username already Exist
+
     public function checkMail($email, $username)
     {
         try {
@@ -43,19 +45,28 @@ class Database
         }
     }
 
+
+// TO register a new user
     public function registerUser($name, $username, $email, $password)
     {
-        $password = md5($password);
+        try {
+            $password = md5($password);
 
-        $sql = 'INSERT INTO `user`(`name`, `username`, `email`, `password`) VALUES (:name, :username, :email, :password)';
-        $query = $this->dbh->prepare($sql);
-        $query->bindParam(':name', $name, PDO::PARAM_STR);
-        $query->bindParam(':username', $username, PDO::PARAM_STR);
-        $query->bindParam(':email', $email, PDO::PARAM_STR);
-        $query->bindParam(':password', $password, PDO::PARAM_STR);
-        $query->execute();
+            $sql = 'INSERT INTO `user`(`name`, `username`, `email`, `password`) VALUES (:name, :username, :email, :password)';
+            $query = $this->dbh->prepare($sql);
+            $query->bindParam(':name', $name, PDO::PARAM_STR);
+            $query->bindParam(':username', $username, PDO::PARAM_STR);
+            $query->bindParam(':email', $email, PDO::PARAM_STR);
+            $query->bindParam(':password', $password, PDO::PARAM_STR);
+            $query->execute();
+        } catch (PDOException $e) {
+            exit('Error :' . $e->getMessage());
+        }
     }
 
+
+
+// to Sign in a user
     public function sign_in($email, $username, $password)
     {
         $password = md5($password);
@@ -86,93 +97,142 @@ class Database
         
     }
 
+
+    // To log out a user
     public function log_out()
     {
-        session_start();
-        $_SESSION = array();
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-            setcookie(
-                session_name(),
-                '',
-                time() - 60 * 60,
-                $params["path"],
-                $params["domain"],
-                $params["secure"],
-                $params["httponly"]
-            );
+        try {
+            session_start();
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(
+                    session_name(),
+                    '',
+                    time() - 60 * 60,
+                    $params["path"],
+                    $params["domain"],
+                    $params["secure"],
+                    $params["httponly"]
+                );
+            }
+            unset($_SESSION['login']);
+            session_destroy(); // destroy session
+            header("location:index.php"); 
+        } catch (PDOException $e) {
+            exit('Error :' . $e->getMessage());
         }
-        unset($_SESSION['login']);
-        session_destroy(); // destroy session
-        header("location:index.php"); 
     }
 
+    // To Upload a New File
     public function upload($status, $user_id)
     {
-        if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK) {
-            
-            $fileName = $_FILES['uploadedFile']['name'];
-            $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
-            $fileSize = $_FILES['uploadedFile']['size'];
-            $fileType = $_FILES['uploadedFile']['type'];
-            $fileNameCmps = explode(".", $fileName);
-            $fileExtension = strtolower(end($fileNameCmps));
- 
+        try {
+            if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK) {
 
-            $rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
-            // echo var_dump($rootDir . "\File-Management\document");
+                $fileName = $_FILES['uploadedFile']['name'];
+                $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+                $fileSize = $_FILES['uploadedFile']['size'];
+                $fileType = $_FILES['uploadedFile']['type'];
+                $fileNameCmps = explode(".", $fileName);
+                $fileExtension = strtolower(end($fileNameCmps));
 
-            $target_dir = $rootDir. "\Projects\File-Management\document\\";
-            $target_file = $target_dir . basename($fileName);
-            $uploadOk = 1;
-            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-            if (move_uploaded_file($fileTmpPath, $target_file)) {
-                
-                $name = $fileName;
-                $user_id = $user_id;
-                $status = $status;
+                $rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
+                // echo var_dump($rootDir . "\File-Management\document");
 
-                $sql = 'INSERT INTO `file`(`user_id`, `name`, `status`) VALUES (:user_id, :name, :status)';
-                $query = $this->dbh->prepare($sql);
-                $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-                $query->bindParam(':name', $name, PDO::PARAM_STR);
-                $query->bindParam(':status', $status, PDO::PARAM_STR);
-                $query->execute();
-                return "The file " . htmlspecialchars(basename($fileTmpPath)) . $status . $user_id . " has been uploaded.";
-                
-            } else {
-                return "Sorry, there was an error uploading your file.";
+                $target_dir = $rootDir . "\Projects\File-Management\document\\";
+                $target_file = $target_dir . basename($fileName);
+                $uploadOk = 1;
+                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+                if (move_uploaded_file($fileTmpPath, $target_file)) {
+
+                    $name = $fileName;
+                    $user_id = $user_id;
+                    $status = $status;
+
+                    $sql = 'INSERT INTO `file`(`user_id`, `name`, `status`) VALUES (:user_id, :name, :status)';
+                    $query = $this->dbh->prepare($sql);
+                    $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+                    $query->bindParam(':name', $name, PDO::PARAM_STR);
+                    $query->bindParam(':status', $status, PDO::PARAM_STR);
+                    $query->execute();
+                    return "The file " . htmlspecialchars(basename($fileTmpPath)) . $status . $user_id . " has been uploaded.";
+                } else {
+                    return "Sorry, there was an error uploading your file.";
+                }
             }
-           
+        } catch (PDOException $e) {
+            exit('Error :' . $e->getMessage());
         }
 
         
     }
 
 
-
+// To Get A File's Name From The Database
     public function getAllFiles()
     {
-        $sql = 'SELECT * FROM file';
-        $query = $this->dbh->prepare($sql);
-        $query->execute();
-        $result = $query->fetchALL(PDO::FETCH_OBJ);
-        $dt = json_encode($result);
-        return $dt;
+        try {
+            $sql = 'SELECT * FROM file';
+            $query = $this->dbh->prepare($sql);
+            $query->execute();
+            $result = $query->fetchALL(PDO::FETCH_OBJ);
+            $dt = json_encode($result);
+            return $dt;
+        } catch (PDOException $e) {
+            exit('Error :' . $e->getMessage());
+        }
+
+        
     }
 
-
+// To send Access Request
     public function sendRequest($recieve_id, $sent_id, $file_id, $reason)
     {
 
-        $sql = 'INSERT INTO `request`( `receive_id`, `file_id`, `sent_id`, `reason`) VALUES (:receive_id, :file_id, :sent_id, :reason)';
-        $query = $this->dbh->prepare($sql);
-        $query->bindParam(':receive_id', $recieve_id, PDO::PARAM_STR);
-        $query->bindParam(':file_id', $file_id, PDO::PARAM_STR);
-        $query->bindParam(':sent_id', $sent_id, PDO::PARAM_STR);
-        $query->bindParam(':reason', $reason, PDO::PARAM_STR);
-        $query->execute();
+        try {
+            $sql = 'INSERT INTO `request`( `receive_id`, `file_id`, `sent_id`, `reason`) VALUES (:receive_id, :file_id, :sent_id, :reason)';
+            $query = $this->dbh->prepare($sql);
+            $query->bindParam(':receive_id', $recieve_id, PDO::PARAM_STR);
+            $query->bindParam(':file_id', $file_id, PDO::PARAM_STR);
+            $query->bindParam(':sent_id', $sent_id, PDO::PARAM_STR);
+            $query->bindParam(':reason', $reason, PDO::PARAM_STR);
+            $query->execute();
+        } catch
+        (PDOException $e) {
+            exit('Error :' . $e->getMessage());
+        }
+    }
+
+// To get Request all request for a user from the data base
+    public function getRequest($user_id){
+        try {
+            $sql = 'SELECT * FROM `request` WHERE receive_id = :receive_id OR sent_id = :sent_id';
+            $query = $this->dbh->prepare($sql);
+            $query->bindParam(':receive_id', $user_id, PDO::PARAM_STR);
+            $query->bindParam(':sent_id', $user_id, PDO::PARAM_STR);
+            $query->execute();
+            $result = $query->fetchALL(PDO::FETCH_OBJ);
+            $result = json_encode($result);
+            return $result;
+        } catch (PDOException $e) {
+            exit('Error :' . $e->getMessage());
+        }
+    }
+
+    public function sendReply($id, $reply)
+    {
+        try {
+            $sql = 'UPDATE `request` SET reply = :reply WHERE id = :id';
+            $query = $this->dbh->prepare($sql);
+            $query->bindParam(':id', $id, PDO::PARAM_STR);
+            $query->bindParam(':reply', $reply, PDO::PARAM_STR);
+            $query->execute();
+        } catch (PDOException $e) {
+            exit('Error :' . $e->getMessage());
+        }
     }
 
 }
--- user/request.php
+++ user/request.php
@@ -8,19 +8,27 @@ if (strlen($_SESSION['alogin']) == 0) {
     header('location:index.php');
 } else {
 
+    if (isset($_POST['reply'])) {
+        
+        $id = $_POST['id'];
+        $reply = $_POST['reply'];
+
+        $data->sendReply($id, $reply);
+    }
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<?php
+    <?php
     require_once "includes/header.php";
     ?>
 
 
-<body>
+    <body>
 
-    <?php
+        <?php
 
         require_once "includes/navbar.php";
 
@@ -32,51 +40,107 @@ if (strlen($_SESSION['alogin']) == 0) {
 
         ?>
 
-   
-    <div class="container" style="margin-top:30px">
-        <div class="row">
-            <div class="col-sm-12 ">
+
+        <div class="container" style="margin-top:30px">
+            <div class="row">
+                <div class="col-sm-12 ">
 
 
 
 
 
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        
-                    </tbody>
-                </table>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>File Name</th>
+                                <th>Reason</th>
+                                <th>Sender</th>
+                                <th>Reply</th>
+                                <th>Status</th>
+                                <th>Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <?php
+                            $rq = $data->getRequest($_SESSION['id']);
+                            $rq = json_decode($rq);
+                            $cnt = 1;
+                            foreach ($rq as $value) {
+                            ?>
+                                <tr>
+                                    <td><?= $cnt ?></td>
+                                    <td><?= $value->file_id ?></td>
+                                    <td><?= $value->reason ?></td>
+                                    <td><?= $value->sent_id ?></td>
+                                    <td><?= (!$value->reply) ? 'Pending' : $value->reply ?></td>
+                                    <td><?= (!$value->reply) ? 'Pending' : 'sent' ?></td>
+                                    <td><?= date("Y-m-d", strtotime($value->date)) ?></td>
+                                    <td><a href="#" class="align-self-center ml-3" data-toggle="modal" data-target="#reply"><img src="../images/reply.svg" alt="reply" /></a></td>
+                                </tr>
+                                <!-- Modal -->
+                                <div class="modal fade" id="reply" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="staticBackdropLabel">Send Request</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+
+                                            <!-- Default form request -->
+                                            <div class="modal-bodytext-center border border-light p-5">
+                                                <form class="text-center" action="request.php" method="POST">
+
+                                                <input type="text" name="id" value="<?= $value->id?>" hidden>
+
+                                                    <div class="form-group">
+                                                        <button type="submit" class="btn btn-success btn-lg btn-block" name="reply" value="accept">Accept</button>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <button type="submit" class="btn btn-danger btn-lg btn-block" name="reply" value="reject">Reject</button>
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                            <!-- Default form register -->
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
+                                $cnt += 1;
+                            }
+                            ?>
+
+
+                        </tbody>
+                    </table>
 
 
 
 
+
+
+                </div>
             </div>
         </div>
-    </div>
 
-    <?php
+        <?php
         require_once "includes/footer.php";
         ?>
 
-    <script>
+        <script>
 
-    </script>
+        </script>
 
-</body>
+    </body>
 
-</html><?php
+    </html><?php
         } ?>
